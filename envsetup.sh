fbb415c3f5206b1611efd8ea339a80745d615b2d
Adhitya Mohan <adhitya@yaosp.dev>
2021-11-04 11:53:43 +0100

    envsetup: Fixup roomservice derp
    *OOps

--- envsetup.sh
+++ envsetup.sh
@@ -711,16 +711,16 @@ function lunch()
 
     if ! check_product $product
     then
-        # if we can't find a product, try to grab it off the LineageOS GitHub
+        # if we can't find a product, try to grab it off the YAAP GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lineage/build/tools/roomservice.py $product
+        vendor/yaap/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lineage/build/tools/roomservice.py $product true
+        vendor/yaap/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
